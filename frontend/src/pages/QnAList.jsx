46e7d04926f3abd455c745f5ee208e5182b63e6e
Abhinav Kumar <abhinavkr123@yahoo.in>
2025-07-14 10:30:32 +0530

    Fix: import axios from 'axios' in QnAList.jsx to resolve build error

--- frontend/src/pages/QnAList.jsx
+++ frontend/src/pages/QnAList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from '../utils/api';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 const QnAList = () => {
